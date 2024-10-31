803e16401e3b93aa2b25f184ad25c4847ba8298a
jhbodamer <johnhbodamer@gmail.com>
2024-10-30 23:08:08 -0400

    Starting savings account class

--- Assignment2/accounts.py
+++ Assignment2/accounts.py
@@ -8,3 +8,21 @@ class BankAccount:
             self.balance += amount
         else:
             raise Exception("Please select a positive number")
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            raise Exception("Not enough funds, select a lower amount")
+
+    def check_balance(self):
+        return self.balance
+
+    def __str__(self):
+        return(f"This bank account has {self.balance} dollars in it")
+
+class SavingsAccount:
+    def __init__(self, initial_balance=0, interest_rate=0.02):
+        self.balance = initial_balance
+        self.interest = interest_rate
+        
