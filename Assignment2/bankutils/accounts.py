b21bb99f99aea71401f70707a9574de3360ceeb3
jhbodamer <johnhbodamer@gmail.com>
2024-10-31 01:33:56 -0400

    Finished comments

--- Assignment2/bankutils/accounts.py
+++ Assignment2/bankutils/accounts.py
@@ -1,5 +1,7 @@
 # accounts.py
+
 class BankAccount:
+    # initialize and test for valid parameter
     def __init__(self, initial_balance=0):
         self.balance = 0
         try:
@@ -11,6 +13,7 @@ class BankAccount:
         finally:
             print("Have a nice day")
 
+    # deposit if amount is positive
     def deposit(self, amount=0.00):
         try:
             x = testpos(amount)
@@ -19,6 +22,7 @@ class BankAccount:
         except ValueError:
             print("Deposit can't be negative")
 
+    # withdraw if amount is positive and less than or equal to balance
     def withdraw(self, amount):
         try:
             x = testsufficient(amount, self.balance)
@@ -28,14 +32,17 @@ class BankAccount:
         except ValueError:
             print("Insufficient Funds / Negative Amount")
 
+    # simple function
     def check_balance(self):
         return self.balance
 
+    # defines how object is converted to string
     def __str__(self):
         return f"This bank account has {self.balance} dollars in it"
 
 
 class SavingsAccount:
+    # initialize and check for valid parameters
     def __init__(self, initial_balance=0, interest_rate=0.02):
         self.balance = 0
         try:
@@ -53,6 +60,7 @@ class SavingsAccount:
         except ValueError:
             print("Interest Rate can't be negative")
 
+    # deposit if amount is positive
     def deposit(self, amount):
         try:
             x = testpos(amount)
@@ -61,6 +69,7 @@ class SavingsAccount:
         except ValueError:
             print("Deposit can't be negative")
 
+    # withdraw if amount is positive and less than or equal to balance
     def withdraw(self, amount):
         try:
             x = testsufficient(amount, self.balance)
@@ -70,21 +79,26 @@ class SavingsAccount:
         except ValueError:
             print("Insufficient Funds / Negative Amount")
 
+    # update balance to reflect interest
     def apply_interest(self):
         self.balance *= 1 + self.interest
 
+    # defines how object is converted to string
     def __str__(self):
         return f"This is a savings account with {self.balance} dollars and an interest rate of {self.interest}"
+
+    # simple function
     def check_balance(self):
         return self.balance
 
-
+# used in both classes to raise error if value is negative and return true otherwise
 def testpos(amount):
     if amount < 0:
         raise ValueError()
     else:
         return(True)
 
+# used for withdraws to see if the amount is less than the balance
 def testsufficient(amount, balance):
     if amount > balance:
         raise ValueError()
--- Assignment2/main_program.py
+++ Assignment2/main_program.py
@@ -1,3 +1,4 @@
+# main_program.py
 # Import Module
 from bankutils import *
 
@@ -9,6 +10,7 @@ account.deposit(50) # successful deposit
 account.deposit(-1) # negative deposit error
 print(account)# Expected balance: $350
 account.withdraw(500)  # Expected: "Insufficient balance"
+account.withdraw(-1) # Negative withdraw amount
 account.withdraw(100) # successful withdraw
 print(account)  # Expected balance: $250
 print(account.check_balance()) # testing check_balance function
@@ -20,6 +22,7 @@ savings.deposit(100) # Expected balance: $300
 savings.deposit(-2) # Negative deposit
 savings.withdraw(1000) # Insufficient funds
 savings.withdraw(55) # Successful withdraw
+savings.withdraw(-1) # Negative withdraw amount
 print(savings) # Check values $245
 savings.apply_interest() # Expected balance after interest: $257.25
 savings.apply_interest() # Expected balance after interest: $270.11
