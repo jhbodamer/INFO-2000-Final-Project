e16d7cd3f56bd80c17dfcbbd7e3f30a5046f5254
jhbodamer <johnhbodamer@gmail.com>
2024-11-28 13:29:35 -0500

    Setup some of the stuff to make the pygame library work, decided I'm gonna do a 360 pong instead of normal pong to make it unique

--- Final Project/main.py
+++ Final Project/main.py
@@ -3,13 +3,29 @@ import pygame
 
 # create classes ill come back to
 
+#set the size of the window and make screen object
+w = 400
+h = 400
+window = pygame.display.set_mode((w,h))
+
+#pygame setup stuff
+clock = pygame.time.Clock()
+# fill window with white color
+window.fill((255,255,255), (0,0,w,h))
+
 
 class Ball:
     def __init__(self, size, speed):
         self.radius = size
         self.speed = speed
 
+
 class Paddle:
     def __init__(self, length, speed):
         self.length = length
         self.speed = speed
+
+# looking at examples online, pygame needs a loop to run
+while True:
+    # this sets how fast the game will run
+    clock.tick(100)
