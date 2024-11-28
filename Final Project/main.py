56e3c76cad56e614e638aca2980f5c287afcfd51
jhbodamer <johnhbodamer@gmail.com>
2024-11-28 14:08:49 -0500

    successfully drew arc which will become paddle!

--- Final Project/main.py
+++ Final Project/main.py
@@ -1,5 +1,6 @@
 # simple pong game
 import pygame
+import math
 
 # create classes ill come back to
 
@@ -7,11 +8,13 @@ import pygame
 w = 400
 h = 400
 window = pygame.display.set_mode((w,h))
-
+#make a center as this game is based on a circle
+centerx = w/2
+centery = h/2
 #pygame setup stuff
 clock = pygame.time.Clock()
 # fill window with white color
-window.fill((255,255,255), (0,0,w,h))
+window.fill((255,255,255))
 
 
 class Ball:
@@ -21,16 +24,30 @@ class Ball:
 
 
 class Paddle:
-    def __init__(self, length, speed):
+    def __init__(self, length, speed, radius, angle):
         self.length = length
         self.speed = speed
 
+        self.radius = radius
+        self.angle = angle
+
+    # make a class to draw paddle
+    def draw(self):
+        # using the draw arc function in python
+        pygame.draw.arc(window, (0, 0, 0), pygame.Rect(centerx-self.radius, centery-self.radius, self.radius*2,
+        self.radius*2), self.angle, self.angle+self.length)
 # looking at examples online, pygame needs a loop to run
 while True:
     # this sets how fast the game will run
     clock.tick(100)
-
+    #update screen
+    pygame.display.flip()
     #need this to prevent crashing
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit(0)
+
+    #create test paddle obj
+    test = Paddle(math.pi, 0 , 100, 0)
+    test.draw()
+    pygame.draw.circle(window, (0,0,0), (centerx,centery), 5)
