31bf2bff5fddf61f7f81cab61792b89c549c5052
jhbodamer <johnhbodamer@gmail.com>
2024-11-28 13:06:09 -0500

    Made barebones classes, planning on using same paddle class for both player and cpu

--- Final Project/main.py
+++ Final Project/main.py
@@ -1,1 +1,15 @@
-test 1
+# simple pong game
+import pygame
+
+# create classes ill come back to
+
+
+class Ball:
+    def __init__(self, size, speed):
+        self.radius = size
+        self.speed = speed
+
+class Paddle:
+    def __init__(self, length, speed):
+        self.length = length
+        self.speed = speed
